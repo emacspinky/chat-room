83f810e11371c62a513599ca288f7ec782bd8d0f
mvf4z7 <mike.fanger@gmail.com>
2015-04-17 15:38:40 -0500

    cleaned up client code

--- client/client.c
+++ client/client.c
@@ -36,42 +36,41 @@ int main( int argc, char* argv[] )
  
     if( argc != 2 ) 
     { 
-	printf( "Usage: %s hostname\n", argv[0] ); 
-	exit(1); 
-    } 
+        printf( "Usage: %s hostname\n", argv[0] );
+        exit(1); 
+    }
  
     /* get the host */ 
     if( ( hp = gethostbyname( argv[1] ) ) == NULL ) 
-    { 
-	printf( "%s: %s unknown host\n", argv[0], argv[1] ); 
-	exit( 1 ); 
+    {
+        printf( "%s: %s unknown host\n", argv[0], argv[1] );
+        exit( 1 ); 
     } 
     memcpy( hp->h_addr_list[0], (char*)&server_addr.sin_addr, hp->h_length );
  
     /* create a socket */ 
     if( ( sd = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1 ) 
-    { 
-	perror( "client: socket failed" ); 
-	exit( 1 ); 
+    {
+        perror( "client: socket failed" );
+        exit( 1 ); 
     } 
  
     /* connect a socket */ 
-    if( connect( sd, (struct sockaddr*)&server_addr, 
-		 sizeof(server_addr) ) == -1 ) 
+    if( connect( sd, (struct sockaddr*)&server_addr, sizeof(server_addr) ) == -1 )
     { 
-	perror( "client: connect FAILED:" ); 
-	exit( 1 ); 
+        perror( "client: connect FAILED:" );
+        exit( 1 ); 
     } 
  
     printf("connect() successful! will send a message to server\n"); 
     printf("Input a string:\n" ); 
  
     while( gets(buf) != NULL)
-    { 
-     write(sd, buf, sizeof(buf)); 
-      read(sd, buf, sizeof(buf)); 
-      printf("SERVER ECHOED: %s\n", buf); 
-    } 
+    {
+        write(sd, buf, sizeof(buf));
+        read(sd, buf, sizeof(buf));
+        printf("SERVER ECHOED: %s\n", buf);
+    }
  
     close(sd); 
     return(0); 
